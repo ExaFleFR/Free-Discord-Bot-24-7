fd5aeb05862ecae2803c5a8da9a988982d93cc80
EXA <101008457+ExaFleFR@users.noreply.github.com>
2022-03-10 15:39:34 +0100

    Update Bot discord création1.py

--- Bot discord création1.py
+++ Bot discord création1.py
@@ -1,14 +1,14 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="-")
+bot = commands.Bot(command_prefix="YOU PREFIXE")
 
 @bot.event
 async def on_ready():
     print("I'm alive")
 
-@bot.command(hey)
+@bot.command()
 async def ping(ctx):
     await ctx.send("pong")
 
-bot.run("OTUxNDg0MTY3NTIxMjAyMjM4.YioIrw.-8SBhLHx1GGiCp1pioUwcbfSCDg")
+bot.run("YOUR TOKEN BOT")
